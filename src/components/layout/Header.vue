0a1982b82789f6484e8b9e52cca4136a15a5069c
edenbenbow <edenbenbow@users.noreply.github.com>
2020-03-06 14:18:32 -0800

    Project w/basic features

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<header class="header">
+    <h1>To-do list</h1>
+</header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header {
+        text-align: left;
+        color: #324AB2;
+        padding-top: 10px;
+    }
+</style>
